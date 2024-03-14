6045937fc4f6f4f980fe431d7d54d64a8fe58557
ghostofsin <badukoro@gmail.com>
2024-03-14 13:11:08 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+    appDir: true,
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"]
+  },
 	webpack: (config) => {
     config.resolve.fallback = {
       "mongodb-client-encryption": false ,
       "aws4": false
+    },
+		config.experiments = {
+      topLevelAwait: true,
+      layers: true,
     };
 
     return config;
