a5412c841f1f7746acf11a5dcb17a00a15de141b
ghostofsin <badukoro@gmail.com>
2024-03-14 13:32:38 +0800

    fix middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,4 +5,11 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
 	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+	runtime: 'edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    // allows a single file
+    '/lib/utilities.js',
+    // use a glob to allow anything in the function-bind 3rd party module
+    '/node_modules/mongoose/**',
+  ],
 };
